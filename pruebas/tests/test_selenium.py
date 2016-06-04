d6a984c28f69a51fe701851fd1343066cc35d7a9
Juan Pablo Camejo <jp.came@gmail.com>
2016-06-04 22:39:53 +0000

    intentando configurar chrome y chrome driver en travis

--- pruebas/tests/test_selenium.py
+++ pruebas/tests/test_selenium.py
@@ -2,8 +2,14 @@ import unittest
 from selenium import webdriver
 
 class PythonOrgSearch(unittest.TestCase):
-    def testMethod(self):
+    def testSearhWithFirefox(self):
         browser = webdriver.Firefox()
+        browser.get('http://www.python.org')
+        self.assertEqual('Welcome to Python.org', browser.title)
+        body = browser.find_element_by_css_selector('body')
+        self.assertIn('python', body.text)
+    def testSearhWithChrome(self):
+        browser = webdriver.Chrome()
         browser.get('http://www.python.org')
         self.assertEqual('Welcome to Python.org', browser.title)
         body = browser.find_element_by_css_selector('body')
